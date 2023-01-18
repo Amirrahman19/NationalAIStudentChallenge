846775f059772a180b51619644d8db47bd302e23
Amirrahman <19.amir.rahman@gmail.com>
2023-01-18 21:37:54 +0800

    adjusted setup.py

--- setup.py
+++ setup.py
@@ -5,18 +5,18 @@ import pyautogui
 import time
 import socket
 
-foods = [
-	'AI-Beehoon', 'AI-TehTarik', 'AI-CurryPuff', 'AI-MeeGoreng', 'AI-DuckRice', 'AI-CurryRice',
-	'AI-CharSiu', 'AI-HokkienMee', 'AI-BakKutTeh', 'AI-CharKwayTeow', 'AI-ChaiTowKway', 'AI-FishballSoup',
-	'AI-KayaToast', 'AI-Meepok', 'AI-AyamPenyet', 'AI-IceKachang', 'AI-MiloDinosaur', 'AI-ChilliGrab',
-	'AI-CerealPrawn', 'AI-Banmian', 'AI-ChickenRice', 'AI-Satay', 'AI-OrganSoup', 'AI-PepperCrab'
-]
+# foods = [
+# 	'AI-Beehoon', 'AI-TehTarik', 'AI-CurryPuff', 'AI-MeeGoreng', 'AI-DuckRice', 'AI-CurryRice',
+# 	'AI-CharSiu', 'AI-HokkienMee', 'AI-BakKutTeh', 'AI-CharKwayTeow', 'AI-ChaiTowKway', 'AI-FishballSoup',
+# 	'AI-KayaToast', 'AI-Meepok', 'AI-AyamPenyet', 'AI-IceKachang', 'AI-MiloDinosaur', 'AI-ChilliGrab',
+# 	'AI-CerealPrawn', 'AI-Banmian', 'AI-ChickenRice', 'AI-Satay', 'AI-OrganSoup', 'AI-PepperCrab'
+# ]
 
 comp_id = int(socket.gethostname().split('-')[1])
-name = foods[comp_id]
+# name = foods[comp_id]
 
 code = re.search("<div><p>(.*)</p></div>", requests.get('http://rentry.co/spaiTetris').text, re.IGNORECASE)
-
+# https://spai-human-counter-backend-api.herokuapp.com/latest/fc6/9`
 assert code
 code = code.group(1)
 
@@ -31,8 +31,8 @@ time.sleep(1)
 pyautogui.moveTo(739, 583)
 time.sleep(0.5)
 pyautogui.click()
-pyautogui.write(name)
-pyautogui.moveTo(739, 682)
+# pyautogui.write(name)
+# pyautogui.moveTo(739, 682)
 time.sleep(0.5)
 pyautogui.click()
 pyautogui.moveTo(968, 632)
