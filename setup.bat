fd70081d5ff10c9d5f6af9135893a44d978437ad
Amir Rahman <19.amir.rahman@gmail.com>
2023-01-27 12:36:25 +0800

    adjusted setup.py and bat file

--- setup.bat
+++ setup.bat
@@ -1,9 +1,8 @@
-set PATH=%path%;C:\ProgramData\Anaconda3\condabin;C:\ProgramData\Anaconda3\Scripts;C:\ProgramData\Anaconda3\Library\bin;
-set PATH=%path%;C:\cygwin64\bin;
-cd C:\Users\Student\Desktop\Tetris-AI-main
-call activate base
-pip install -r "requirements.txt"
+@REM set PATH=%path%;C:\ProgramData\Anaconda3\condabin;C:\ProgramData\Anaconda3\Scripts;C:\ProgramData\Anaconda3\Library\bin;
+@REM set PATH=%path%;C:\cygwin64\bin;
+cd C:\Users\amir_\Onedrive - Singapore Polytechnic\VSC\PROGRAMMING\PYTHON\Python\SPAIHUMANCOUNTER\Human_Counter\human_counter
+call activate human-entrance
 python setup.py
-python main.py
+call peekingduck run
 pause
 call conda deactivate
