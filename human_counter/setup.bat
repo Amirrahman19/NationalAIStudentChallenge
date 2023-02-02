305776376aba2e1f5bb7fd43a1b8ab93eaf40287
Amirrahman <19.amir.rahman@gmail.com>
2023-02-03 00:59:46 +0800

    reconfigured setup bat file

--- human_counter/setup.bat
+++ human_counter/setup.bat
@@ -1,8 +1,9 @@
-@REM set PATH=%path%;C:\ProgramData\Anaconda3\condabin;C:\ProgramData\Anaconda3\Scripts;C:\ProgramData\Anaconda3\Library\bin;
-@REM set PATH=%path%;C:\cygwin64\bin;
-cd C:\Users\amir_\Onedrive - Singapore Polytechnic\VSC\PROGRAMMING\PYTHON\Python\SPAIHUMANCOUNTER\Human_Counter\human_counter
-call activate human-entrance
-python setup.py
-call peekingduck run
-pause
-call conda deactivate
+set root=C:\Users\amir_\anaconda3
+
+call %root%\Scripts\activate.bat %root%
+
+call conda activate human-entrance
+
+call peekingduck run 
+
+@REM pause
